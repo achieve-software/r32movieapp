a587f99ebe0462062020114ae6b9f04274aae316
Achieve-software <vtcbcdtyr@gmail.com>
2023-08-05 16:53:54 +0300

    card

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -5,7 +5,8 @@ const Main = () => {
   const { movies} = useContext(MovieContext); 
   
   return (
-  <div><div className="flex justify-center flex-wrap">
+  <div>
+    <div className="flex justify-center flex-wrap">
 {movies.map((movie)=> (
   <MovieCard key={movie.id}  {...movie}/>
 ))}</div>    </div>    );
