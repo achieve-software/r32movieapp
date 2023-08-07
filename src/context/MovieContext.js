5d496ec203aa64204e0dabfe1a1bc94cb95d069b
Achieve-software <vtcbcdtyr@gmail.com>
2023-08-07 16:17:53 +0300

    moviecard

--- src/context/MovieContext.js
+++ src/context/MovieContext.js
@@ -4,17 +4,24 @@ export const MovieContext = createContext();
 const API_KEY = "90060517ce2cf29a4077c6c03a513ec9";
 const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
 const MovieContextProvider = ({ children }) => {
-  const [movies, setMovies] = useState([]);  useEffect(() => {
+  const [movies, setMovies] = useState([]);  
+  const [loading, setLoading] = useState(false);
+  
+  
+  
+  useEffect(() => {
     getMovies(FEATURED_API);
   }, []);
   console.log(movies);  
   
   const getMovies = (API) => {
+    setLoading(true)
     axios
       .get(API)
       .then((res) => setMovies(res.data.results))
-      .catch((err) => console.log(err));
-  }; 
+      .catch((err) => console.log(err))
+      .finally(()=> setLoading(false));
+  };
   
   const values = { movies, getMovies };  return (
     <MovieContext.Provider value={values}>{children}</MovieContext.Provider>
